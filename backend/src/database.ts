12155826c33f2fc494c16987eec83ff06972a68c
ThanhTT <thanhtt@splus-software.com.vn>
2019-10-07 23:38:41 +0700

    commit

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,14 +1,18 @@
 import { Pool } from 'pg'
 import * as fs from 'fs'
+import * as path from 'path'
 
 const PB = new Pool()
 
 export default PB
 
 export function databaseInit() {
-  const query = fs.readFileSync('./sql/skynet_script.sql').toString()
+  const query = fs.readFileSync(path.resolve(__dirname, 'sql/skynet_script.sql')).toString()
   PB.query(query, (err, res) => {
-    if (err) throw err
-    console.log('DATABASE INIT COMPLETE')
+    if (err) {
+		console.log('Skipping database init')
+	} else {
+		console.log('DATABASE INIT COMPLETE')
+	}
   })
 }
